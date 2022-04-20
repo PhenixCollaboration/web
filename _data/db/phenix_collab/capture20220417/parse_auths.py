10586319f8212f70d38b23ac5d1e119f8c26003c
buddhasystem <potekhin@bnl.gov>
2022-04-20 14:31:09 -0400

    Finished the parsing script for members, corrected the initial list for special characters, generated master file for the table on site

--- _data/db/phenix_collab/capture20220417/parse_auths.py
+++ _data/db/phenix_collab/capture20220417/parse_auths.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
-authors = open('authsmod.csv','r')
+authors_file = open('authsmod.csv', 'r')
+primary_file = open('primary.csv',  'r')
 
-lines = authors.readlines()
+authors = authors_file.readlines()
+primary = primary_file.readlines()
 
-for line in lines:
+primary_lookup = {}
+for line in primary:
     content = line.strip()
     elements = content.split(',')
-    print((',').join(elements[3:]))
+    primary_lookup[elements[0]+','+elements[1]] = elements[3]
+
+for line in authors:
+    content = line.strip()
+    elements = content.split(',')
+    output_line = elements[:4]
+
+    if len(elements)>4:
+        try:
+            output_line[3] = primary_lookup[elements[0]+','+elements[1]]
+        except:
+            print('Primary Institution lookup ERROR')
+
+    print(','.join(output_line))
